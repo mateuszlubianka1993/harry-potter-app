d2152e95aa42d83b2c7cadbc2f1df9b8349f1e86
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-03 13:13:04 +0200

    add BrowserRouter and Routes to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './Header';
 import Spells from './Spells';
 import Houses from './Houses';
@@ -7,8 +8,15 @@ import Characters from './Characters';
 class App extends React.Component {
     render() {
         return(
-            <div>
-                <Header />
+            <div className="app-container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route path="/spells" component={Spells} />
+                        <Route path="/houses" component={Houses} />
+                        <Route path="/characters" component={Characters} />
+                    </div>
+                </BrowserRouter>
             </div>
         );
     };
