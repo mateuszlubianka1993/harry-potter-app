4a6405eca5be332f8d3316af987b181d357c8ac6
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-05 15:29:27 +0200

    add header to Characters component

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -3,7 +3,16 @@ import React from 'react';
 class Characters extends React.Component {
     render() {
         return(
-            <div>Characters</div>
+            <div>
+                <div className="container">
+                <div className="page-header">
+                    <h2>Characters</h2>
+                </div>
+                <div className="page-list">
+                    
+                </div>
+            </div>
+            </div>
         );
     };
 }
