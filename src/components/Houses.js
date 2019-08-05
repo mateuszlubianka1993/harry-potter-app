36a20293a49878b5b5d7988f91efd1356554f36e
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-05 13:56:39 +0200

    add header to Houses component

--- src/components/Houses.js
+++ src/components/Houses.js
@@ -11,19 +11,19 @@ class Houses extends React.Component {
     renderHouses() {
         return this.props.houses.map(house=> {
             return(
-                <div className="card" style={{width:'450px'}}>
+                <div className="card" style={{width:'450px'}} key={house._id}>
                     <div className="card-header">
                         <h5 className="card-title">{house.name}</h5>
-                        <p className="blockquote-footer">{house.values.map(value=> <span>{`${value} `}</span>)}</p>
+                        <p className="blockquote-footer">{house.values.map(value=> <span key={value}>{`${value} `}</span>)}</p>
                     </div>
                     <div className="card-body">
                         <p className="card-text">Mascot: {house.mascot}</p>
                         <p className="card-text">Head of house: {house.headOfHouse}</p>
                         <p className="card-text">House ghost: {house.houseGhost}</p>
                         <p className="card-text">Founder: {house.founder}</p>
-                        <p className="card-text">Colors: {house.colors.map(color=><span style={{color: `${color}`}}>{`${color} `}</span>)}</p>
+                        <p className="card-text">Colors: {house.colors.map(color=><span style={{color: `${color}`}} key={color}>{`${color} `}</span>)}</p>
                     </div>
-                    <div class="card-footer text-muted">
+                    <div className="card-footer text-muted">
                         {house.school}
                     </div>
                 </div>
@@ -34,7 +34,12 @@ class Houses extends React.Component {
     render() {
         return(
             <div className="container">
-                {this.renderHouses()}
+                <div className="page-header">
+                    <h2>Hogwarts Houses</h2>
+                </div>
+                <div className="page-list">
+                    {this.renderHouses()}
+                </div>
             </div>
         );
     };
