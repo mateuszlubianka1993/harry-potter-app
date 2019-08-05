34dcb7c3c71660c718372fb9181180f46e6f7b10
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-05 12:13:14 +0200

    use getHouses action and houses state in Houses component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import harryPAPI from '../apis/harryPAPI';
-import hp-api from '../apis/hp-api';
+import hp_api from '../apis/hp-api';
 
 const api_key = '$2a$10$Qkw7sVZaDU8vi0MG3NjK6upi.nIKVJrJ8UcQQtk8GG3WolbshN.M6';
 
@@ -8,5 +8,5 @@ export const getHouses = () => async dispatch => {
         params: {key: api_key}
     });
     const result = response.data;
-    dispatch({type: GET_HOUSES, payload: result})
+    dispatch({type: 'GET_HOUSES', payload: result})
 };
--- src/components/Houses.js
+++ src/components/Houses.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {getHouses} from '../actions';
 
 class Houses extends React.Component {
+
+    componentDidMount() {
+        this.props.getHouses();
+    };
+
     render() {
+        console.log(this.props.houses);
         return(
             <div>Houses</div>
         );
     };
 }
 
-export default Houses;
+const mapStateToProps = (state) => {
+    return {
+        houses: state.houses
+    };
+};
+
+export default connect(mapStateToProps, {getHouses}) (Houses);
