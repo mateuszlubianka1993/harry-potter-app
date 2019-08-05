e165bed6590f6a3d1fb2ca932da038a09d4c3c06
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-05 14:03:43 +0200

    add spellsReducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import housesReducer from './housesReducer';
+import spellsReducer from './spellsReducer';
 
 export default combineReducers({
     houses: housesReducer,
+    spells: spellsReducer
 });
