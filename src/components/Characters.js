866e4a80f5eec9358756491b5160150111982b80
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-05 16:08:51 +0200

    display characters in Characters component

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -6,7 +6,23 @@ class Characters extends React.Component {
     componentDidMount() {
         this.props.getCharacters();
     };
+
     render() {
+        const {characters} = this.props;
+        const charactersList = characters.map(character=>{
+            return(
+                <div className="card" key={character.name} style={{width: '350px'}}>
+                    <img src={character.image} className="card-img-top" alt="character"/>
+                    <div className="card-body">
+                        <h5 className="card-title">{`${character.name} `}<span class="badge badge-secondary">{character.species}</span></h5>
+                        <h6>{`House: ${character.house} `}<span class="badge badge-secondary">{character.hogwartsStudent ? 'student' : 'staff'}</span></h6>
+                        <h6>{character.dateOfBirth}</h6>
+                        <p className="card-text">Patronus: {character.patronus}</p>
+                        <p className="card-text">Actor: {character.actor}</p>
+                    </div>
+                </div>
+            );
+        });
         return(
             <div>
                 <div className="container">
@@ -14,7 +30,7 @@ class Characters extends React.Component {
                     <h2>Characters</h2>
                 </div>
                 <div className="page-list">
-                    
+                    {charactersList}
                 </div>
             </div>
             </div>
