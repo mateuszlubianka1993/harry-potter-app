dafd95815e37ea322815971c98c5ce7311726662
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-05 11:56:19 +0200

    add getHouses action

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import harryPAPI from '../apis/harryPAPI';
+import hp-api from '../apis/hp-api';
+
+const api_key = '$2a$10$Qkw7sVZaDU8vi0MG3NjK6upi.nIKVJrJ8UcQQtk8GG3WolbshN.M6';
+
+export const getHouses = () => async dispatch => {
+    const response = await harryPAPI.get('/houses', {
+        params: {key: api_key}
+    });
+    const result = response.data;
+    dispatch({type: GET_HOUSES, payload: result})
+};
