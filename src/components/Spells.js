1cb475dfe04e98496b3e4c905ff48805c1ad701e
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-05 15:02:00 +0200

    display spells in Spells component

--- src/components/Spells.js
+++ src/components/Spells.js
@@ -9,13 +9,26 @@ class Spells extends React.Component {
     };
 
     render() {
+        const {spells} = this.props;
+        const spellsList = spells.map(spell=>{
+            return(
+                <div class="card" key={spell._id}>
+                    <div class="card-body">
+                        <h5 class="card-title">{spell.spell}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Type: {spell.type}</h6>
+                        <p class="card-text">Effect: {spell.effect}</p>
+                    </div>
+                </div>
+            );
+        });
+
         return(
             <div className="container">
                 <div className="page-header">
                     <h2>List of spells</h2>
                 </div>
                 <div className="page-list">
-                    
+                    {spellsList}
                 </div>
             </div>
         );
