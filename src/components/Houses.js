a2e164512cf4d640fbf060b3dd658f891e51b416
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-06 15:47:23 +0200

    add House.css

--- src/components/Houses.js
+++ src/components/Houses.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {getHouses} from '../actions';
 import '../styles/page.css';
+import '../styles/Houses.css';
 
 class Houses extends React.Component {
 
@@ -12,9 +13,9 @@ class Houses extends React.Component {
     renderHouses() {
         return this.props.houses.map(house=> {
             return(
-                <div className="card" style={{width:'450px'}} key={house._id}>
+                <div className="card house-card" key={house._id}>
                     <div className="card-header">
-                        <h5 className="card-title">{house.name}</h5>
+                        <h5 className="card-title house-name">{house.name}</h5>
                         <p className="blockquote-footer">{house.values.map(value=> <span key={value}>{`${value} `}</span>)}</p>
                     </div>
                     <div className="card-body">
