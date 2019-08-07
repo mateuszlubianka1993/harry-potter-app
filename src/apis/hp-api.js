45c21863f9e5851341a0035382caf5b1225dc1e5
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-07 14:07:54 +0200

    change http to https in hp-api.js

--- src/apis/hp-api.js
+++ src/apis/hp-api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // https://hp-api.herokuapp.com/
 export default axios.create({
-    baseURL: 'http://hp-api.herokuapp.com/api'
+    baseURL: 'https://hp-api.herokuapp.com/api'
 });
