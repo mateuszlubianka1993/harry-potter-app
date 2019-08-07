93751a7e8468b18873bbd434ebc7e4f11ea0cd47
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-07 12:17:30 +0200

    add RWD

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import '../styles/Header.css';
 const Header = () => {
     return(
         <div className="header-container">
-            <h1>Harry Potter World</h1>
+            <h1 className="header-txt">Harry Potter World</h1>
         </div>
     );
 };
