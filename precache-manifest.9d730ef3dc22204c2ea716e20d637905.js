70c70730bedecfe8721c3de21ff13b3347e99436
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-08-07 14:03:41 +0200

    Updates

---
+++ precache-manifest.9d730ef3dc22204c2ea716e20d637905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fec979f22d68506579ff9f94caa83f",
+    "url": "/harry-potter-app/index.html"
+  },
+  {
+    "revision": "3aa786da46710395bd3b",
+    "url": "/harry-potter-app/static/css/main.e3367a29.chunk.css"
+  },
+  {
+    "revision": "25bac12a1dff180bda08",
+    "url": "/harry-potter-app/static/js/2.b12e8062.chunk.js"
+  },
+  {
+    "revision": "3aa786da46710395bd3b",
+    "url": "/harry-potter-app/static/js/main.b179f419.chunk.js"
+  },
+  {
+    "revision": "d3c4d80aa05624b2a72d",
+    "url": "/harry-potter-app/static/js/runtime~main.5e027c50.js"
+  },
+  {
+    "revision": "f7b1d2632b4fbaac4aea695880411f24",
+    "url": "/harry-potter-app/static/media/b-k-HAl6CKxM3xU-unsplash-sm.f7b1d263.jpg"
+  }
+]);
